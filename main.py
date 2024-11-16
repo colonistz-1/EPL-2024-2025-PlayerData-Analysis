cac4ed87bde85e1cd23c3b2215ef6dbf6de1ff9c
colonistz-1 <tony.nguyen2415@gmail.com>
2024-11-15 19:59:08 -0800

    Shot Shopping Data

--- main.py
+++ main.py
@@ -7,20 +7,19 @@ df1 = pd.read_csv("data\epl-players-expectedasistsper90.csv")
 df2 = pd.read_csv("data\epl-players-prgPper90.csv")
 
 # %%
-df1_new_header = df1.iloc[0] 
-df1 = df1[1:]  
-df1.columns = df1_new_header 
+def set_first_row_as_header(df):
+    new_header = df.iloc[0]
+    df = df[1:].copy()
+    df.columns = new_header
+    df.reset_index(drop=True, inplace=True)
+    return df
 
-df1.reset_index(drop=True, inplace=True)
+# %%
+df1 = set_first_row_as_header(df1)
 
 
 # %%
-df2_new_header = df2.iloc[0] 
-df2 = df2[1:]  
-df2.columns = df2_new_header 
-
-df2.reset_index(drop=True, inplace=True)
-
+df2 = set_first_row_as_header(df2)
 
 # %%
 df1.head()
@@ -69,7 +68,7 @@ pass_fig.update_layout(
             yref='paper',
             showarrow=False,
             x=0.5,  
-            y=1.05,  
+            y=1.1,  
         )
     ],
     xaxis_title='Expected Assists per90',
@@ -78,7 +77,42 @@ pass_fig.update_layout(
     yaxis_title_font=dict(size=18),
 )
 
-pass_fig.write_html("index.html") 
+# %%
+epl_keepers = pd.read_csv('data\epl-keepers-save-statistics.csv')
+epl_keepers = set_first_row_as_header(epl_keepers)
+epl_keepers = df = epl_keepers.iloc[1:].reset_index(drop=True)
 
+# %%
+epl_keepers["90s"] = epl_keepers["90s"].astype(float)
+epl_keepers["Save%"] = epl_keepers["Save%"].astype(float)
+epl_keepers["SoTA"] = epl_keepers["SoTA"].astype(float)
+
+# %%
+epl_keepers["Save%/90s"] = epl_keepers["Save%"] / epl_keepers["90s"]
+epl_keepers["SoTA/90s"] = epl_keepers["SoTA"] / epl_keepers["90s"]
+
+# %%
+gk_fig = px.scatter(x=epl_keepers["SoTA/90s"], y=epl_keepers["Save%/90s"] , title="Shots Against and Saves", text=epl_keepers['Player'])
+gk_fig.update_layout(
+    title_font=dict(size=30),
+    title_x=0.48,
+    annotations = [
+        dict(
+            text='Premier League 2024/25, GW11, Minimum 400 Minutes Played',
+            xref='paper', 
+            yref='paper',
+            showarrow=False,
+            x=0.5,  
+            y=1.1,  
+        )
+    ],
+    xaxis_title='Shots On Target Against',
+    xaxis_title_font=dict(size=18),
+    yaxis_title='Save%',
+    yaxis_title_font=dict(size=18),
+)
+with open('index.html', 'a') as f:
+    f.write(pass_fig.to_html(full_html=False, include_plotlyjs='cdn'))
+    f.write(gk_fig.to_html(full_html=False, include_plotlyjs='cdn'))
 
 
